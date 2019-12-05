ff72d48205e0aab76c25bd5d48d36e40a3b82519
blckbearx <58561253+blckbearx@users.noreply.github.com>
2019-12-05 11:52:19 -0300

    Update pkgi_config.h

--- include/pkgi_config.h
+++ include/pkgi_config.h
@@ -2,12 +2,12 @@
 
 #include "pkgi_db.h"
 
-typedef struct Config {
-    DbSort sort;
-    DbSortOrder order;
-    uint32_t filter;
-    int no_version_check;
-} Config;
+//typedef struct Config {
+//    DbSort sort;
+//    DbSortOrder order;
+//    uint32_t filter;
+//    int no_version_check;
+//} Config;
 
 void pkgi_load_config(Config* config, char* update_url, uint32_t update_len);
 void pkgi_save_config(const Config* config, const char* update_url);
