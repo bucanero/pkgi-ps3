a7ffc93c1ff240f913086abba72871d7b684ee44
Damian Parrino <dparrino@gmail.com>
2020-06-22 22:09:19 -0300

    Use SHA256 from PolarSSL

--- include/pkgi_sha256.h
+++ include/pkgi_sha256.h
@@ -1,16 +1,11 @@
 #pragma once
 
 #include "pkgi_utils.h"
+#include <polarssl/sha2.h>
 
-#define SHA256_BLOCK_SIZE 64
 #define SHA256_DIGEST_SIZE 32
 
-typedef struct {
-    uint8_t  buffer[SHA256_BLOCK_SIZE] GCC_ALIGN(16);
-    uint32_t state[8];
-    uint64_t count;
-} sha256_ctx;
-
-void sha256_init(sha256_ctx* ctx);
-void sha256_update(sha256_ctx* ctx, const uint8_t* buffer, uint32_t size);
-void sha256_finish(sha256_ctx* ctx, uint8_t* digest);
+#define sha256_ctx			sha2_context
+#define sha256_init(ctx)	sha2_starts(ctx, 0)
+#define sha256_update		sha2_update
+#define sha256_finish		sha2_finish
